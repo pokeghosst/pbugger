9205442d21edd88526e69136b8e7b5f4fecd5dc9
pokeghost <pokeghost@disroot.org>
2025-08-30 19:05:38 +0300

    fix: Clean up and address CLang warnings

--- pbugger.cpp
+++ pbugger.cpp
@@ -7,17 +7,14 @@
 class Debugger {
 public:
     Debugger() {
-        h_process = NULL;
-        pid = 0;
-        debugger_active = false;
+        h_process_ = nullptr;
+        pid_ = 0;
+        debugger_active_ = false;
     }
 
     bool load(const std::wstring &path_to_exe) {
-        // Determines how to create the process
-        DWORD creation_flags = DEBUG_PROCESS;
-
-        STARTUPINFO startup_info = {0};
-        PROCESS_INFORMATION process_info = {0};
+        STARTUPINFO startup_info = {};
+        PROCESS_INFORMATION process_info = {};
 
         startup_info.dwFlags = STARTF_USESHOWWINDOW;
         startup_info.wShowWindow = SW_HIDE;
@@ -30,14 +27,14 @@ public:
         std::wstring cmd_line = path_to_exe;
 
         if (CreateProcess(
-            NULL, // Application name (NULL to use command line)
+            nullptr, // Application name (NULL to use command line)
             reinterpret_cast<LPSTR>(&cmd_line[0]), // Command line (modifiable)
-            NULL, // Process security attributes
-            NULL, // Thread security attributes
+            nullptr, // Process security attributes
+            nullptr, // Thread security attributes
             FALSE, // Inherit handles
-            creation_flags, // Creation flags
-            NULL, // Environment
-            NULL, // Current directory
+            DEBUG_PROCESS, // Determines how to create the process
+            nullptr, // Environment
+            nullptr, // Current directory
             &startup_info, // Startup info
             &process_info // Process information
         )) {
@@ -46,9 +43,9 @@ public:
 
             // Obtain a valid handle to the newly created process
             // and store it for future access
-            h_process = open_process(process_info.dwProcessId);
-            pid = process_info.dwProcessId;
-            debugger_active = true;
+            h_process_ = open_process(process_info.dwProcessId);
+            pid_ = process_info.dwProcessId;
+            debugger_active_ = true;
 
             // Close the handles from CreateProcess since we have our own
             CloseHandle(process_info.hProcess);
@@ -64,28 +61,25 @@ public:
         }
     }
 
-    HANDLE open_process(DWORD pid) {
-        HANDLE h_process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-        return h_process;
+    static HANDLE open_process(const DWORD pid) {
+        return OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
     }
 
-    HANDLE open_thread(DWORD thread_id) {
-        HANDLE h_thread = OpenThread(THREAD_ALL_ACCESS, FALSE, thread_id);
-
-        if (h_thread != NULL) {
+    static HANDLE open_thread(const DWORD thread_id) {
+        if (const auto h_thread = OpenThread(THREAD_ALL_ACCESS, FALSE, thread_id); h_thread != nullptr) {
             return h_thread;
-        } else {
-            std::cout << "[*] Unable to open the thread.\n";
-            return NULL;
         }
+
+        std::cout << "[*] Unable to open the thread.\n";
+        return nullptr;
     }
 
-    std::vector<DWORD> enumerate_threads() {
+    [[nodiscard]] std::vector<DWORD> enumerate_threads() const {
         std::vector<DWORD> thread_list;
 
         // TH32CS_SNAPTHREAD signals that we want to gather all the threads currently registered in the snapshot
         // We don't care about th32ProcessID because we determine whether the thread belongs to our process ourselves
-        HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+        const auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
 
         if (snapshot == INVALID_HANDLE_VALUE) {
             std::cout << "[*] Failed to take snapshot of threads.\n";
@@ -97,7 +91,7 @@ public:
 
         if (Thread32First(snapshot, &thread_entry)) {
             do {
-                if (thread_entry.th32OwnerProcessID == pid) {
+                if (thread_entry.th32OwnerProcessID == pid_) {
                     thread_list.push_back(thread_entry.th32ThreadID);
                 }
             } while (Thread32Next(snapshot, &thread_entry));
@@ -107,13 +101,12 @@ public:
         return thread_list;
     }
 
-    CONTEXT get_thread_context(DWORD thread_id) {
-        CONTEXT context;
-        memset(&context, 0, sizeof(CONTEXT));
+    static CONTEXT get_thread_context(const DWORD thread_id) {
+        CONTEXT context = {};
         context.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS;
 
-        HANDLE h_thread = open_thread(thread_id);
-        if (h_thread == NULL) {
+        const auto h_thread = open_thread(thread_id);
+        if (h_thread == nullptr) {
             std::cout << "[*] Could not get a handle to the thread.\n";
             return context; // Returns empty context
         }
@@ -128,72 +121,70 @@ public:
         return context;
     }
 
-    bool attach(DWORD pid) {
-        h_process = open_process(pid);
+    bool attach(const DWORD pid) {
+        h_process_ = open_process(pid);
 
         if (DebugActiveProcess(pid)) {
-            debugger_active = true;
-            this->pid = pid;
+            debugger_active_ = true;
+            pid_ = pid;
             run();
             return true;
-        } else {
-            std::cout << "[*] Unable to attach to the process.\n";
-            return false;
         }
+
+        std::cout << "[*] Unable to attach to the process.\n";
+        return false;
     }
 
-    void run() {
-        while (debugger_active == true) {
+    void run() const {
+        while (debugger_active_ == true) {
             get_debug_event();
         }
     }
 
-    void get_debug_event() {
-        DEBUG_EVENT debug_event = {0};
-        DWORD continue_status = DBG_CONTINUE;
+    static void get_debug_event() {
+        DEBUG_EVENT debug_event = {};
 
         if (WaitForDebugEvent(&debug_event, INFINITE)) {
             ContinueDebugEvent(
                 debug_event.dwProcessId,
                 debug_event.dwThreadId,
-                continue_status
+                DBG_CONTINUE
             );
         }
     }
 
     bool detach() {
-        if (DebugActiveProcessStop(pid)) {
+        if (DebugActiveProcessStop(pid_)) {
             std::cout << "[*] Finished debugging. Exiting...\n";
             return true;
-        } else {
-            std::cout << "There was an error\n";
-            return false;
         }
+
+        std::cout << "There was an error\n";
+        return false;
     }
 
     ~Debugger() {
-        if (debugger_active) {
+        if (debugger_active_) {
             detach();
         }
-        if (h_process != NULL) {
-            CloseHandle(h_process);
+
+        if (h_process_ != nullptr) {
+            CloseHandle(h_process_);
         }
     }
 
 private:
-    HANDLE h_process;
-    DWORD pid;
-    bool debugger_active;
+    HANDLE h_process_;
+    DWORD pid_;
+    bool debugger_active_;
 };
 
 int main() {
-    Debugger debugger;
-
     std::cout << "[*] Enter PID to attach to: ";
     DWORD pid;
     std::cin >> pid;
 
-    if (!debugger.attach(pid)) {
+    if (Debugger debugger; !debugger.attach(pid)) {
         std::cout << "[*] Failed to attach to process." << std::endl;
     }
 
